7e19d21aba65c11818bb681420c7c85bce2543f9
Samuele Dassatti <samuele@igloo.ooo>
2020-11-23 00:53:16 +0100

    Mobile UI

--- src/App.js
+++ src/App.js
@@ -33,9 +33,47 @@ export default class App extends Component {
     super(props);
 
     uptimeLog = generateRandomUptimes();
+
+    this.state = {
+      width: false,
+      scrollTop: 0,
+    };
+  }
+
+  debounce = (func, wait) => {
+    let timeout;
+
+    return (...args) => {
+      const later = () => {
+        timeout = null;
+
+        func(...args);
+      };
+
+      clearTimeout(timeout);
+
+      timeout = setTimeout(later, wait);
+    };
+  };
+
+  updateDimensions = () => {
+    if (window.outerWidth !== this.state.width) {
+      this.setState({ width: window.outerWidth });
+    }
+  };
+
+  componentDidMount() {
+    this.updateDimensions();
+    window.addEventListener("resize", this.debounce(this.updateDimensions));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.debounce(this.updateDimensions));
   }
 
   render() {
+    const { width, scrollTop } = this.state;
+
     // remove when actual data is available
     const online = uptimeLog[29].uptime === 100;
     const loading = false;
@@ -51,17 +89,22 @@ export default class App extends Component {
         style={{
           height: "100%",
           backgroundColor: online ? "#0057cb" : "#f44336",
+          overflowY: "auto",
         }}
         className="notSelectable defaultCursor"
+        onScroll={({ target: { scrollTop } }) => this.setState({ scrollTop })}
       >
         <Typography
-          variant="h1"
+          variant={width > 752 ? "h1" : width > 448 ? "h2" : "h3"}
           style={{
             color: "white",
             height: "256px",
+            width: "100%",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
+            position: "absolute",
+            opacity: 1 - scrollTop / (width > 752 ? 64 : 96),
           }}
         >
           {loading ? (
@@ -79,18 +122,28 @@ export default class App extends Component {
         <Paper
           style={{
             width: "100%",
-            height: "calc(100% - 256px)",
-            borderRadius: "32px 32px 0 0",
+            minHeight: "calc(100% - 256px)",
+            borderRadius:
+              (1 - scrollTop / 256) * 32 +
+              "px " +
+              (1 - scrollTop / 256) * 32 +
+              "px 0 0",
             boxShadow:
               "0px -5px 5px -3px rgba(0,0,0,0.2), 0px -8px 10px 1px rgba(0,0,0,0.14), 0px -3px 14px 2px rgba(0,0,0,0.12)",
+            marginTop: "256px",
           }}
           elevation={8}
         >
-          <div style={{ maxWidth: "1080px", margin: "0 auto" }}>
-            <Grid container style={{ padding: "96px 0" }}>
-              <Grid item xs>
+          <div
+            style={{ maxWidth: "1080px", margin: "0 auto", padding: "0 48px" }}
+          >
+            <Grid
+              container
+              style={{ padding: width > 752 ? "96px 0" : "48px 0" }}
+            >
+              <Grid item xs={width > 752 ? 4 : 12} style={{ margin: "16px 0" }}>
                 <Typography
-                  variant="h2"
+                  variant={width > 960 || width < 752 ? "h2" : "h3"}
                   style={{ ...percentageStyle, fontWeight: "bold" }}
                 >
                   {loading ? (
@@ -100,15 +153,15 @@ export default class App extends Component {
                   )}
                 </Typography>
                 <Typography
-                  variant="h5"
+                  variant={width > 960 || width < 752 ? "h5" : "h6"}
                   style={{ ...percentageStyle, fontWeight: 300 }}
                 >
                   {loading ? <Skeleton /> : "Last 24 hours"}
                 </Typography>
               </Grid>
-              <Grid item xs>
+              <Grid item xs={width > 752 ? 4 : 12} style={{ margin: "16px 0" }}>
                 <Typography
-                  variant="h2"
+                  variant={width > 960 || width < 752 ? "h2" : "h3"}
                   style={{ ...percentageStyle, fontWeight: "bold" }}
                 >
                   {loading ? (
@@ -124,15 +177,15 @@ export default class App extends Component {
                   )}
                 </Typography>
                 <Typography
-                  variant="h5"
+                  variant={width > 960 || width < 752 ? "h5" : "h6"}
                   style={{ ...percentageStyle, fontWeight: 300 }}
                 >
                   {loading ? <Skeleton /> : "Last 7 days"}
                 </Typography>
               </Grid>
-              <Grid item xs>
+              <Grid item xs={width > 752 ? 4 : 12} style={{ margin: "16px 0" }}>
                 <Typography
-                  variant="h2"
+                  variant={width > 960 || width < 752 ? "h2" : "h3"}
                   style={{ ...percentageStyle, fontWeight: "bold" }}
                 >
                   {loading ? (
@@ -147,7 +200,7 @@ export default class App extends Component {
                   )}
                 </Typography>
                 <Typography
-                  variant="h5"
+                  variant={width > 960 || width < 752 ? "h5" : "h6"}
                   style={{ ...percentageStyle, fontWeight: 300 }}
                 >
                   {loading ? <Skeleton /> : "Last 30 days"}
@@ -156,14 +209,24 @@ export default class App extends Component {
             </Grid>
             <Grid container>
               <Grid item xs>
-                <Typography variant="h5" style={{ fontWeight: "bold" }}>
+                <Typography
+                  variant={width > 448 ? "h5" : "h6"}
+                  style={{
+                    fontWeight: "bold",
+                    fontSize: width > 352 ? "" : "1.15rem",
+                  }}
+                >
                   API uptime
                 </Typography>
               </Grid>
               <Grid item xs>
                 <Typography
-                  variant="h5"
-                  style={{ textAlign: "right", fontWeight: 300 }}
+                  variant={width > 448 ? "h5" : "h6"}
+                  style={{
+                    textAlign: "right",
+                    fontWeight: 300,
+                    fontSize: width > 352 ? "" : "1.15rem",
+                  }}
                 >
                   Last 30 days
                 </Typography>
@@ -172,8 +235,9 @@ export default class App extends Component {
             <Grid
               container
               style={{
-                margin: "16px -2px 64px -2px",
+                margin: "16px -2px 0",
                 width: "calc(100% + 4px)",
+                paddingBottom: "64px",
               }}
             >
               {uptimeLog.map(({ date, uptime }) => (
@@ -204,9 +268,14 @@ export default class App extends Component {
                           ? "#ffc804"
                           : "#f44336",
                       height: "24px",
-                      width: "calc(calc(100% - 120px)/30)",
+                      width:
+                        "calc((100% - " +
+                        (width > 752 ? 30 : 15) * 4 +
+                        "px)/" +
+                        (width > 752 ? 30 : 15) +
+                        ")",
                       borderRadius: "4px",
-                      margin: "0 2px",
+                      margin: "2px",
                     }}
                   />
                 </Tooltip>
